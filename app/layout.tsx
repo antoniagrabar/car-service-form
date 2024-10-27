4fb625aeb183916c6ac6c98c8f699a5d8c4e096d
Antonia Grabar <antonia.grabar@gmail.com>
2024-10-27 15:25:20 +0100

    Move fonts and vectors to public folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import "./globals.css";
 
 const satoshi = localFont({
-  src: "./fonts/Satoshi-Variable.ttf",
+  src: "../public/fonts/Satoshi-Variable.ttf",
   variable: "--font-satoshi",
   weight: "100 200 300 400 500 600 700 800 900",
 });
